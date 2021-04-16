b38357f3d1a7635aa30cc5ddd8d06b685cacfa94
陈威明 <1329705883@qq.com>
2021-04-16 17:26:37 +0800

    controller  》》index show 404 前端页面

--- src/main/java/cn/hstc/homework/controller/IndexController.java
+++ src/main/java/cn/hstc/homework/controller/IndexController.java
@@ -10,9 +10,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping({"/","/index"})
+    @GetMapping({"/","/index","/index.html"})
     public String toIndex(){
         return  "index";
     }
+
+
+    @GetMapping({"/show","/show.html"})
+    public String toShow(){
+        return  "show";
+    }
+
+    @GetMapping({"/list","/list.html"})
+    public String toList(){
+        return  "list";
+    }
+
+    @GetMapping({"/404","/404.html"})
+    public String to404(){
+        return  "404";
+    }
+
 
 }
